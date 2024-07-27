00005b440fcad3d5e2f419321b1155fb9ebc30ca
Yunna Yap Yoon En <1019yoonen@gmail.com>
2024-07-27 17:10:05 +0800

    user statistics

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -4,6 +4,8 @@ import com.sky.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.Map;
+
 @Mapper
 public interface UserMapper {
 
@@ -14,4 +16,11 @@ public interface UserMapper {
 
     @Select("select * from user where id = #{id}")
     User getById(Long id);
+
+    /**
+     * User Statistics
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -2,8 +2,10 @@ package com.sky.service.impl;
 
 import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
+import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +24,8 @@ import java.util.Map;
 public class ReportServiceImpl implements ReportService {
     @Autowired
     private OrderMapper orderMapper;
+    @Autowired
+    private UserMapper userMapper;
 
     /**
      * Get Turnover Statistics
@@ -58,6 +62,48 @@ public class ReportServiceImpl implements ReportService {
         return TurnoverReportVO.builder()
                 .dateList(StringUtils.join(dateList, ","))
                 .turnoverList(StringUtils.join(turnoverList, ","))
+                .build();
+    }
+
+    /**
+     * Get User Statistics
+     * @param begin
+     * @param end
+     * @return
+     */
+    @Override
+    public UserReportVO getUserStatistics(LocalDate begin, LocalDate end) {
+        // store all date between begin and end into list
+        List<LocalDate> dateList = new ArrayList<>();
+        List<Integer> totalUserList = new ArrayList<>();
+        List<Integer> newUserList = new ArrayList<>();
+        while(!begin.isAfter(end)) {
+            dateList.add(begin);
+
+            // get total user and new user amount by date
+            LocalDateTime beginDateTime = LocalDateTime.of(begin, LocalTime.MIN);
+            LocalDateTime endDateTime = LocalDateTime.of(begin, LocalTime.MAX);
+
+            Map map = new HashMap<>();
+
+            // get total user (all user create before the end date)
+            map.put("end", endDateTime);
+            Integer totalUser = userMapper.countByMap(map);
+            totalUserList.add(totalUser);
+
+            // get new user (all user create between the begin and end date)
+            map.put("begin", beginDateTime);
+            Integer newUser = userMapper.countByMap(map);
+            newUserList.add(newUser);
+
+            // add begin by 1 day
+            begin = begin.plusDays(1);
+        }
+
+        return UserReportVO.builder()
+                .dateList(StringUtils.join(dateList, ","))
+                .totalUserList(StringUtils.join(totalUserList, ","))
+                .newUserList(StringUtils.join(newUserList, ","))
                 .build();
     }
 }
